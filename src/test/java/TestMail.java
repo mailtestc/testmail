b0b0fb1a04424830470394e4717bbb030b9fcce6
Vlad <epsy17@yandex.ru>
2018-06-18 03:25:17 +0300

    first commit

--- src/test/java/TestMail.java
+++ src/test/java/TestMail.java
@@ -1,9 +1,7 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeTest;
