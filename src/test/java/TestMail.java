2d605dd8f70e8a1be80b7915f276c2d4a2ba870c
Vlad <epsy17@yandex.ru>
2018-06-18 04:41:57 +0300

    first commit

--- src/test/java/TestMail.java
+++ src/test/java/TestMail.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -7,18 +8,27 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static junit.framework.TestCase.assertTrue;
+
 
 public class TestMail {
     private static ChromeDriver driver;
+    // Задаем адрес почтового сервера
     String url = "https://mail.ru/";
+    // Задаем логин и пароль
     String emailLogin = "testcsi";
     String emailPass = "123456qW";
+    // Задаем ожидаемые значения отправителя, темы письма и часть содержимого письма
     String expSubject = "Test mail";
     String expSender = "Тест Тестов <testcsi@mail.ru>";
     String expBody = "Lorem ipsum";
 
     @BeforeTest
     public void s00_LaunchBrowser() {
+        // Запуск Chrome
         System.setProperty("webdriver.chrome.driver", "src/ChromeDriver.exe");
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("start-maximized");
@@ -27,7 +37,7 @@ public class TestMail {
 
     @Test
     public void s01_MailRuOpen() {
-
+        // Открытие страницы Mail.ru
         driver.get(url);
         String currentUrl = driver.getCurrentUrl();
 
@@ -41,50 +51,55 @@ public class TestMail {
 
     @Test
     public void s02_Login() {
-//      Login
+        // Авторизация почтового ящика
         WebElement login = driver.findElementById("mailbox:login");
         login.sendKeys(emailLogin);
         WebElement pass = driver.findElementById("mailbox:password");
         pass.sendKeys(emailPass);
         WebElement submit = driver.findElementById("mailbox:submit");
         submit.click();
-        new WebDriverWait(driver, 20);
+        WebElement pageloaded = (new WebDriverWait(driver, 5)).until(ExpectedConditions.elementToBeClickable(By.className("b-datalist__item__wrapper")));
 
-        if (ExpectedConditions.elementToBeClickable(By.className("b-datalist__item__subj")) == null) {
-            System.out.println("Step02 Login FAILED");
-        } else {
+        // Проверка загрузки страницы Входящие
+
+        if (driver.getTitle().contains("Входящие")) {
             System.out.println("Step02 Login PASSED");
+        } else {
+            System.out.println("Step02 Login FAILED");
         }
     }
 
     @Test
     public void s03_OpenMail() {
-//      Login
+        // Открытие письма
         try {
-            WebElement pageloaded = (new WebDriverWait(driver, 5)).until(ExpectedConditions.elementToBeClickable(By.className("b-datalist__item__wrapper")));
-        } catch (Exception e) {
-        }
-        try {
-            WebElement maillink = driver.findElementByClassName("b-datalist__item__info");
-            maillink.click();
+            // Ожидание загрузки страницы
+            WebElement pageloaded = (new WebDriverWait(driver, 5)).until(ExpectedConditions.presenceOfElementLocated(By.className("b-datalist__item__panel")));
         } catch (Exception e) {
         }
 
-        if (ExpectedConditions.elementToBeClickable(By.className("b-datalist__item__info")) == null) {
+        // Здесь нужен более сложный сценарий выбора требуемого письма. Сейчас реализован поиск по contains темы письма.
+        WebElement subjLink = driver.findElement(By.xpath("//div[@class='b-datalist__item__panel']"));
+        WebElement mailLink = subjLink.findElement(By.xpath("//a[contains(.,'" + expSubject + "')]"));
+
+        mailLink.click();
+
+/*
+        if (ExpectedConditions.visibilityOf(driver.findElement(By.className("b-letter__body"))) == null) {
             System.out.println("Step03 OpenMail FAILED");
         } else {
             System.out.println("Step03 OpenMail PASSED");
-        }
+        }*/
     }
-
+/*
     @Test
     public void s04_CheckMail() {
-        String senderemail, sendername, subject, bodyemail;
+        String senderemail, subject, bodyemail;
         try {
             WebElement pageloaded = (new WebDriverWait(driver, 5)).until(ExpectedConditions.elementToBeClickable(By.className("b-letter__foot__boxz")));
         } catch (Exception e) {
         }
-        subject = driver.findElement(By.className("b-letter__head__subj__text")).getText();
+            subject = driver.findElement(By.className("b-letter__head__subj__text")).getText();
         senderemail = driver.findElement(By.className("b-letter__head__addrs__from")).getText();
         bodyemail = driver.findElement(By.className("b-letter__body")).getText();
 
@@ -113,9 +128,9 @@ public class TestMail {
 
         WebElement newlogin = driver.findElementById("mailbox:login");
         if (newlogin == null) {
-            System.out.println("Step04 CheckMail FAILED");
+            System.out.println("Step04 LogOut FAILED");
         } else {
-            System.out.println("Step04 CheckMail PASSED");
+            System.out.println("Step04 LogOut PASSED");
         }
     }
 
@@ -124,4 +139,4 @@ public class TestMail {
         new WebDriverWait(driver, 15).until(ExpectedConditions.visibilityOf(driver.findElementByClassName("games")));
         driver.close();
     }
-}
+*/}
